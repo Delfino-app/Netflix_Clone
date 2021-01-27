58671adb40e93599ba9b28e2c11a6cd6f11be28c
Delfino-app <delfinoapp@gmail.com>
2021-01-27 11:55:49 +0000

    Click Event

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
 
 function App() {
+
+  const handleClick = () =>{
+
+    console.log("Teste");
+  }
+
+  const handleClickAgain = (name) =>{
+
+      console.log(`Clicked by ${name}`);
+  }
+
   return (
     <div className="App">
-      
+        <h2>My Frist React App</h2>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={() => handleClickAgain('Delfino')}>Click Me Again</button>
     </div>
   );
 }
